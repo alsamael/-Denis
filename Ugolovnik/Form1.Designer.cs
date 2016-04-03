7a4116948a5a7f2c70ab12900036cf8819e15c08
alsamael <al_samael@mail.ru>
2016-04-03 11:22:59 +0400

    Download
    Not your problem

---
+++ Ugolovnik/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Ugolovnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(66, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите соответствующий игре вариант";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(797, 588);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Далее";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1076, 629);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Определятор 9000";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ugolovnik/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ugolovnik
+{
+   
+
+
+    public partial class Form1 : Form
+    {
+
+        DirectoryInfo Voprosi;
+
+        int Categoriya; //категория, которая назначается игре
+        int count_q = 0; // категория вопросов, которые берутся из файлов
+        int n_file = 0; // номер файла
+
+        List<RadioButton> Listrb=new List<RadioButton>(); // список радио-кнопок
+
+        Button b2;
+
+
+        FileInfo[] files;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Voprosi = new DirectoryInfo(Application.StartupPath + @"\" + @"\Voprosi"); //пути к файлам
+            files = Voprosi.GetFiles("*.*");
+            
+
+
+        }
+
+
+        public void LoadTest(int n_file)
+        {
+            
+
+           
+
+
+            string name_file = files[n_file].FullName;
+            name_file = name_file.Substring(name_file.Count() - 10); //имя файла с вопросами
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@name_file, System.Text.Encoding.GetEncoding(1251)); // открывает файл с вопросами
+
+            string line;
+
+            int schk_rb=0;
+
+            // построчное считывание файла
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.StartsWith("Категория")==true)
+                {
+                    // запоминает категорию вопросов
+                    int n_s = line.IndexOf(":");
+                    string categ = line.Substring( n_s+1, (line.Length - 10) );            
+                    count_q = Convert.ToInt32(categ);
+
+                }
+                else
+                {
+                    //создает радио-кнопки с содержанием, взятым из файла
+                    
+                 RadioButton   rb = new RadioButton();
+                    rb.Size = new Size(950, 40);
+                    rb.Text = line.Substring(3, line.Length - 4);
+                    rb.Location = new Point(70, 100 + schk_rb * 55);
+                    rb.Name = "Rbutton"+schk_rb;
+
+                    Listrb.Add(rb);
+                   
+                    this.Controls.Add(rb);
+                    rb.Checked = true;
+                    schk_rb++;
+
+                    button1.Location = new Point(800, 100 + schk_rb * 55);
+
+                }
+                    
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+
+            LoadTest(n_file);
+            
+
+
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            // Обнуляет переменные, чтобы начать тест заново
+            Categoriya=0;
+            count_q = 0;
+            n_file = 0;
+
+            Voprosi = new DirectoryInfo(Application.StartupPath + @"\" + @"\Voprosi");
+            files = Voprosi.GetFiles("*.*");
+
+            Listrb = new List<RadioButton>();
+
+            label1.Location = new Point(66, 50);
+            label1.Text = "Выберите соответствующий игре вариант";
+
+            LoadTest(n_file);
+            button1.Visible = true;
+            b2.Dispose();
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Очищает форму от созданных ранее радио-кнопок
+            for (int j = 0; j < Listrb.Count(); j++)
+            {
+                Controls.Remove(Listrb[j]);
+            }
+
+
+            
+            // проверят, какая радио-кнопка выбрана
+
+            for (int i = 0; i < Listrb.Count(); i++)
+            {
+
+
+                if (i != Listrb.Count() - 1)
+                {
+
+                    if (Listrb[i].Checked == true)
+                    {
+
+                        Categoriya = count_q;
+
+                       // если категорию можно определить сразу же - выводит результат
+
+                        label1.Location = new Point(320, 220);
+                        label1.Text = "Данной игре присваиваится рейтинг "+Categoriya.ToString()+"+.";
+                        button1.Visible = false;
+
+                        // создаёт кнопку для очищения параметров, чтобы можно начать тест занова
+                        b2 = new Button()
+                        { Location = new Point(420, 300), Size = new Size(200, 30), Text = "Начать новый тест", AutoSize = true , BackColor= Color.White};
+                        Controls.Add(b2);
+                        b2.Click += new EventHandler(b2_Click);
+
+                        Listrb = new List<RadioButton>();
+                        break;
+
+                    }
+
+                }
+                else
+                {
+                    //если нет, выводит вопросы из следующего файла с другой возрастной категорией
+
+                    Listrb = new List<RadioButton>();
+
+
+                    n_file++;
+                    // если ни один вопрос, характеризующий определенную категорию игры, не подходит, присваивает 0+
+                    if (n_file >= files.Count())
+                    {
+
+                        label1.Location = new Point(320, 220);
+                        label1.Text = "Данной игре присваиваится рейтинг 0+.";
+                        button1.Visible = false;
+
+                        // создаёт кнопку для очищения параметров, чтобы можно было начать тест занова
+                        b2 = new Button()
+                        { Location = new Point(420, 300), Size = new Size(200, 30), Text ="Начать новый тест", AutoSize=true, BackColor = Color.White };
+                        Controls.Add(b2);
+                        b2.Click += new EventHandler(b2_Click);
+                        
+
+                        break;
+                    }
+
+                    
+                    LoadTest(n_file);
+
+
+
+
+                }
+
+            
+                
+
+            }
+
+           
+
+
+
+
+            
+
+
+
+        }
+    }
+}
